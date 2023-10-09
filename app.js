bd635b0d8f93d643cdbf41ce94e713e957c9c26b
unknown <hafizhammadali08121422@gmail.com>
2023-10-09 21:43:35 +0500

    Assignment 5 have done

---
+++ app.js
@@ -0,0 +1,62 @@
+{document.write("<table> <tr> <th> Answer 1 and 2 </th>")}
+
+{document.write("<th> Answer 3 </th>")}
+
+{document.write("<th> Answer 4</th> </tr>")}
+
+var num1 = 10;
+var num2 = 8;
+
+var total = num1 + num2
+var total = num1 - num2 
+var total = num1 * num2
+var total = num1 / num2
+var total = num1 % num2
+
+{document.write ( "<tr> <td>Sum of 10 and 8 is " + total + "<br> <br>" )}
+
+{document.write ( "Subtraction of 10 and 8 is " + total + "<br> <br>" )}
+
+{document.write ( "Multiplication of 10 and 8 is " + total  + "<br> <br>" )}
+
+{document.write ( "Division of 10 and 8 is " + total  + "<br> <br>" )}
+
+{document.write ( "Modulus of 10 and 8 is " + total  + "<br> <br></td>" )}
+
+
+var undefine 
+var value = 5;
+++value
+var add = value + 7
+--add 
+var divide = add % 3;
+
+{document.write("<td><p>Value after variable declaration is undefined </p>")}
+
+{document.write("<p>Initial value: " + value +"</p>")}
+
+{document.write("Value after increment is: " + value +"</p>")}
+
+{document.write("<p>Value after addition is: " + add +"</p>" )}
+
+{document.write("<p>Value after decerment is: " + add +"</p>" )}
+
+{document.write("<p>The remainder is: " + divide + "</p></td>")}
+
+
+
+var oneTicketCost = 600;
+var bying5Tickets = oneTicketCost * 5;
+
+{document.write("<td>Total cost to buy 5 tickets to a movie is " + bying5Tickets + "PKR</td> </tr> </table>")}
+
+
+
+{document.write(" <h2><a href=./Ans_5.html> Answer 5</a></h2>")}
+
+{document.write(" <h2><a href=./Ans6_and_7.html> Answer 6 and 7</a></h2>")}
+
+{document.write(" <h2><a href=./Ans_8.html> Answer 8</a></h2>")}
+
+{document.write(" <h2><a href=./Ans_9_and_above.html> Answer 9 and Above</a></h2>")}
+
